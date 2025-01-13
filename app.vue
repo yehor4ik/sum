2f8d995cd55a51a5c2e22501bb356beb5c3ae8a4
Yehor Domin <yehor.domin@tentens.tech>
2025-01-13 08:25:13 +0200

    added env constant

--- app.vue
+++ app.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    Hello
   </div>
 </template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig()
+
+onMounted(async () => {
+  const fromDate = new Date(2025, 0, 1).getTime()
+  const toDate = new Date(2025, 0, 12).getTime()
+  const response1 = await fetch(`https://api.monobank.ua/personal/client-info`, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Token": config.public.bankAuthToken
+    },
+  });
+
+  //TODO need get account id from response1
+
+  const accountId = 0
+
+  const response = await fetch(`https://api.monobank.ua/personal/statement/${accountId}/${fromDate}/${toDate}`, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Token":  config.public.bankAuthToken
+    },
+  });
+  console.log('response', await response.json())
+})
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      bankAuthToken: process.env.NUXT_PUBLIC_BANK_AUTH_TOKEN ?? '',
+    }
+  }
 })
